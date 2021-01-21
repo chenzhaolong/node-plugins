7a44b51ef89feff35e7d7a4a958a9c4b5fade0de
chenzhaolong <1332588507@qq.com>
2021-01-22 01:56:02 +0800

    定义高低频LRU算法的入参

--- ssr-cache-plugin/lib/cache.js
+++ ssr-cache-plugin/lib/cache.js
@@ -1,7 +1,34 @@
 /**
  * @file 基于lru的低高频缓存策略
+ * todo： logger的使用
  */
+import LRU from './lru';
+import Monitor from './monitor';
+
+const HF_MAX_LENGTH = 10 * 1000;
+const LF_MAX_LENGTH = 10 * 1000;
+const HF_MAX_AGE = 60 * 1000;
+const LF_MAX_AGE = 60 * 1000;
+const HF_TIMES = 10;
 
 export default class Cache {
+    constructor(options) {
+        const {
+            HFLength = HF_MAX_LENGTH,
+            LFLength = LF_MAX_LENGTH,
+            HFMaxAge = HF_MAX_AGE,
+            LFMaxAge = LF_MAX_AGE,
+            HFTimes = HF_TIMES,
+            onBeforeDelete = () => {},
+            onUpgrade = () => {},
+            onDemotion = () => {},
+            onLogger = () => {} // 待定
+        } = options;
 
+        this.LFLru = new LRU({length: LFLength, maxAge: LFMaxAge, onBeforeDelete});
+        this.HFLru = new LRU({length: HFLength, maxAge: HFMaxAge, onBeforeDelete});
+        this.frequency = HFTimes;
+        this.onUpgrade = onUpgrade;
+        this.onDemotion = onDemotion;
+    }
 }
